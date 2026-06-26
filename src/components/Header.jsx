8c5001b7b9623dd62443c27229fb206d2af6064e
AnjulAryal <aryalanjul9068@gmail.com>
2026-06-26 10:50:31 +0545

    Initial Commit with navbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <div className="navbar bg-base-100 shadow-sm bg-blue-200 p-5 h-[10vh]">
+            <a className="btn btn-ghost text-xl font-bold">HabitTracker</a>
+        </div>
+    )
+}
+
+export default Header;
